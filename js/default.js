49556e768bb267ecf7a610e586ba90b1904b5ead
dzmitry-kankalovich <dzmitry_kankalovich@epam.com>
2014-08-06 12:24:59 +0300

    settings for msm

--- js/default.js
+++ js/default.js
@@ -1,95 +1,206 @@
 var _CQ = {
-    "CQ WCM modes": {
-        "disabled": {
-            "title": "Disabled",
-            "param": "wcmmode=disabled",
-            "icon": "icons/adobe.png"
-        },
-        "preview": {
-            "title": "Preview",
-            "param": "wcmmode=preview",
-            "icon": "icons/preview.png"
-        },
-        "edit": {
-            "title": "Edit",
-            "param": "wcmmode=edit",
-            "icon": "icons/edit.png"
-        }
+  "CQ WCM modes": {
+    "Disabled": {
+      "title": "Disabled",
+      "url": "",
+      "url_path": "",
+      "param": "wcmmode=disabled",
+      "icon": "icons/adobe.png"
     },
-    "Debug": {
-        "layout": {
-            "title": "Layout",
-            "param": "debug=layout",
-            "icon": "icons/debug.png"
-        },
-        "clientlibs": {
-            "title": "Client Libs",
-            "param": "debugClientLibs=true",
-            "icon": "icons/debug.png"
-        }
+    "Preview": {
+      "title": "Preview",
+      "url": "",
+      "url_path": "",
+      "param": "wcmmode=preview",
+      "icon": "icons/preview.png"
     },
-
-    "CQ pages": {
-        "Groovy Console": {
-            "title": "Groovy Console",
-            "url_path": "/etc/groovyconsole.html",
-            "icon": "icons/cq.png"
-        },
-        "packmngr": {
-            "title": "Package manager",
-            "url_path": "/crx/packmgr/index.jsp",
-            "icon": "icons/cq.png"
-        },
-        "loginWCM": {
-            "title": "Login to WCM",
-            "url_path": "/login.html",
-            "icon": "icons/cq.png"
-        },
-        "WCM": {
-            "title": "WCM",
-            "url_path": "/siteadmin",
-            "icon": "icons/cq.png"
-        },
-        "CRX": {
-            "title": "CRX",
-            "url_path": "/crx/explorer",
-            "icon": "icons/cq.png"
-        },
-        "CRXDE": {
-            "title": "CRXDE",
-            "url_path": "/crxde",
-            "icon": "icons/crxde.png"
-        },
-        "console": {
-            "title": "Felix console",
-            "url_path": "/system/console",
-            "icon": "icons/felix.png"
-        },
-        "logs": {
-            "title": "Logs",
-            "url_path": "/bin/crxde/logs",
-            "icon": "icons/felix.png"
-        },
-        "jvm": {
-            "title": "JVM Status",
-            "url_path": "/system/console/jmx/java.lang%3Atype%3DRuntime",
-            "icon": "icons/felix.png"
-        }
-    },
-
-    "CQ Instances": {
-        "local_author": {
-            "title": "local author",
-            "url": "http://localhost:4502"
-        },
-        "local_publish": {
-            "title": "local publish",
-            "url": "http://localhost:4503"
-        }
+    "Edit": {
+      "title": "Edit",
+      "url": "",
+      "url_path": "",
+      "param": "wcmmode=edit",
+      "icon": "icons/edit.png"
     }
+  },
+  "Debug": {
+    "Layout": {
+      "title": "Layout",
+      "url": "",
+      "url_path": "",
+      "param": "debug=layout",
+      "icon": "icons/debug.png"
+    },
+    "Client Libs": {
+      "title": "Client Libs",
+      "url": "",
+      "url_path": "",
+      "param": "debugClientLibs=true",
+      "icon": "icons/debug.png"
+    }
+  },
+  "CQ pages": {
+    "Groovy Console": {
+      "title": "Groovy Console",
+      "url": "",
+      "url_path": "/etc/groovyconsole.html",
+      "param": "",
+      "icon": "icons/cq.png"
+    },
+    "Package manager": {
+      "title": "Package manager",
+      "url": "",
+      "url_path": "/crx/packmgr/index.jsp",
+      "param": "",
+      "icon": "icons/cq.png"
+    },
+    "Login to WCM": {
+      "title": "Login to WCM",
+      "url": "",
+      "url_path": "/login.html",
+      "param": "",
+      "icon": "icons/cq.png"
+    },
+    "WCM": {
+      "title": "WCM",
+      "url": "",
+      "url_path": "/siteadmin",
+      "param": "",
+      "icon": "icons/cq.png"
+    },
+    "CRX": {
+      "title": "CRX",
+      "url": "",
+      "url_path": "/crx/explorer",
+      "param": "",
+      "icon": "icons/cq.png"
+    },
+    "CRXDE": {
+      "title": "CRXDE",
+      "url": "",
+      "url_path": "/crxde",
+      "param": "",
+      "icon": "icons/crxde.png"
+    },
+    "Felix console": {
+      "title": "Felix console",
+      "url": "",
+      "url_path": "/system/console/configMgr",
+      "param": "",
+      "icon": "icons/adobe.png"
+    },
+    "Logs": {
+      "title": "Logs",
+      "url": "",
+      "url_path": "/bin/crxde/logs",
+      "param": "",
+      "icon": "icons/adobe.png"
+    },
+    "JVM Status": {
+      "title": "JVM Status",
+      "url": "",
+      "url_path": "/system/console/jmx/java.lang%3Atype%3DRuntime",
+      "param": "",
+      "icon": "icons/adobe.png"
+    },
+    "Sling logs": {
+      "title": "Sling logs",
+      "url": "",
+      "url_path": "/system/console/status-slinglogs",
+      "param": "",
+      "icon": "icons/adobe.png"
+    },
+    "Translator": {
+      "title": "Translator",
+      "url": "",
+      "url_path": "/libs/cq/i18n/translator.html",
+      "param": "",
+      "icon": "icons/cq.png"
+    },
+    "Replication": {
+      "title": "Replication",
+      "url": "",
+      "url_path": "/etc/replication.html",
+      "param": "",
+      "icon": "icons/cq.png"
+    }
+  },
+  "CQ Instances": {
+    "CI Publish": {
+      "title": "CI Publish",
+      "url": "http://54.72.37.45:8081",
+      "url_path": "/content/moneysupermarket/en_gb/insurance/car-insurance/question-set/results.html#?enquiryId=6f8ef842-ac7d-4758-8279-5d7b270362bd",
+      "param": "",
+      "icon": "icons/siteadmin.png"
+    },
+    "CI Author": {
+      "title": "CI Author",
+      "url": "http://54.72.37.45:8080",
+      "url_path": "/content/moneysupermarket/en_gb/insurance/car-insurance/question-set/results.html#?enquiryId=6f8ef842-ac7d-4758-8279-5d7b270362bd",
+      "param": "",
+      "icon": "icons/siteadmin.png"
+    },
+    "Jenkins": {
+      "title": "Jenkins",
+      "url": "http://54.72.16.117:8080",
+      "url_path": "",
+      "param": "",
+      "icon": "icons/siteadmin.png"
+    },
+    "Frontend CI": {
+      "title": "Frontend CI",
+      "url": "http://54.72.16.117:9000",
+      "url_path": "/#/motorinsurance/results?enquiryId=6f8ef842-ac7d-4758-8279-5d7b270362bd&partialEnquiryId=undefined",
+      "param": "",
+      "icon": "icons/siteadmin.png"
+    },
+    "QA ELB": {
+      "title": "QA ELB",
+      "url": "http://msm-qa-elb-pub-1969091497.eu-west-1.elb.amazonaws.com/car-insurance.html",
+      "url_path": "",
+      "param": "",
+      "icon": "icons/siteadmin.png"
+    },
+    "CI ELB": {
+      "title": "CI ELB",
+      "url": "http://msm-ci-elb-pub-2026406112.eu-west-1.elb.amazonaws.com/car-insurance.html",
+      "url_path": "",
+      "param": "",
+      "icon": "icons/siteadmin.png"
+    },
+    "SIT 1 Publish": {
+      "title": "SIT 1 Publish",
+      "url": "http://54.76.189.18:8081/car-insurance.html",
+      "url_path": "",
+      "param": "",
+      "icon": "icons/siteadmin.png"
+    },
+    "SIT 1 Author": {
+      "title": "SIT 1 Author",
+      "url": "http://54.76.189.18:8080/",
+      "url_path": "",
+      "param": "",
+      "icon": "icons/siteadmin.png"
+    },
+    "SIT 1 ELB": {
+      "title": "SIT 1 ELB",
+      "url": "www.sit1.gb.moneysupermarket.com/car-insurance.html",
+      "url_path": "",
+      "param": "",
+      "icon": "icons/siteadmin.png"
+    }
+  },
+  "Confluence": {
+    "AEM Environments": {
+      "title": "AEM Environments",
+      "url": "https://moneysupermarket.atlassian.net/wiki/display/KS/AEM+Environments",
+      "url_path": "",
+      "param": "",
+      "icon": "icons/siteadmin.png"
+    }
+  }
 };
 
-var _CQ_CHROME_UTILS_BUILD_VER = '1.0.1';
+var _CQ_CHROME_UTILS_BUILD_VER = '1.0.2';
 
 var _SETTINGS_NAME = '_CQ' + '_' + _CQ_CHROME_UTILS_BUILD_VER;
 
